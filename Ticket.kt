489043e95f65b9f067c15db84a5e601d70b5bf33
ilonaSag <kupoles09@gmail.com>
2025-03-21 09:48:28 +0500

    Ticket.kt

--- Ticket.kt
+++ Ticket.kt
@@ -1,17 +1,34 @@
 package  Railway
-class Ticket(var name:String,
-             age:Int,
-var numTrain:Int,
-var station:String,
-var time:Int,
-var data:Int,
-var price:Double,):Passenger(name,age) {
- override fun Info(){
-     println("Имя: $names")
-     println("Возраст: $age")
-     println("Станция: $station")
-     println("Время отбытия: $time")
-     println("Дата отбытия: $data")
-     println("Итоговая цена: $price")
- }
+class Ticket(
+    var name:String,
+    age:Int,
+    var numTrain: Int?,
+    var station:String,
+    var time:Int?,
+    var data:Int?,
+    var price:Double?,):Passenger(name,age) {
+
+    //Функция для вывода информации о билете
+    override fun Info(){
+        println("Имя: $names")
+        println("Возраст: $age")
+        println("Станция: $station")
+        println("Время отбытия: $time")
+        println("Дата отбытия: $data")
+        println("Итоговая цена: $price")
+    }
+    fun ChangeTrainNumber(newNumber: Int) {
+        numTrain = newNumber
+        println("Номер поезда изменен на: $numTrain")
+    }
+    //Рассчет цены билета
+    fun FinalPrice(discount: Double): Double {
+        return price!! * (1 - discount)
+    }
+
+    //Вывод об измененной станции
+    fun СhangeStation(newStation: String) {
+        station = newStation
+        println("Станция изменена на: $station")
+    }
 }
