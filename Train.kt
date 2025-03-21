dc3efe522863f35157009398cd2459d02d260938
ilonaSag <kupoles09@gmail.com>
2025-03-21 09:46:54 +0500

    Train.kt

--- Train.kt
+++ Train.kt
@@ -1,7 +1,19 @@
-package  Railway
-open class Train(numer:String,
-endStation:String,
-interval:String,
-price:Double) {
+package Railway
 
+open class Train(
+    var numer: String,
+    var endStation: String,
+    var interval: String,
+    var price: Double
+) {
+
+    fun СhangeInterval(newInterval: String) {
+        interval = newInterval
+        println("Интервал движения изменен на: $interval")
+    }
+
+    fun СhangePrice(newPrice: Double) {
+        price = newPrice
+        println("Цена билета изменена на: $price")
+    }
 }
