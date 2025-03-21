2d919cc8211a731302fc23e89eb6e0a14de16cb1
ilonaSag <kupoles09@gmail.com>
2025-03-21 09:48:06 +0500

    Passenger.kt

--- Passenger.kt
+++ Passenger.kt
@@ -1,15 +1,22 @@
 package  Railway
-open class Passenger (var names:String,var age : Int) {
+open class Passenger (var names:String,var age : Int?) {
+    //Функция для проверки возраста
     fun ValidAge():Boolean{
-        if (age<=0)
+        if (age!!<=0)
         {
             println("Возраст не может быть меньше 0 или равняться ему")
             return false
         }
         return true
     }
+
+    //Функция для выводы информации пассажира
     open fun Info(){
         println("Имя: $names")
         println("Возраст: $age")
+    }
+    fun ChangeName(newName: String) {
+        names = newName
+        println("Имя изменено на: $names")
     }
 }
